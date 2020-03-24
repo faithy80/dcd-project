6e1f3f5712830b6eb637a3515f4f48efa1263d5e
Ubuntu <ubuntu@ip-172-31-82-51.ec2.internal>
2020-03-24 22:30:14 +0000

    search.html template added. run.py updated to test MongoDB connection.

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html', recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
